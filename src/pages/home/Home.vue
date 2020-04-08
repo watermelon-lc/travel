b9bc71ee1cd69e3a209b36237d173508f097ecf7
lc <lc@anhereef.com>
2020-04-08 19:02:35 +0800

    '开始编写首页的header'

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <HomeHeader></HomeHeader>
+  </div>
+</template>
+
+<script>
+import HomeHeader from './components/Header.vue'
+export default {
+  name: 'Home',
+  components: { HomeHeader }
+}
+</script>
+
+<style>
+
+</style>
